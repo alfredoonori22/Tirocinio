d20813d2d2f0d47a5377c71088e33bac6e1c405d
alfredoonori22 <alfredoonori45@gmail.com>
2024-01-16 01:28:15 +0100

    Updated script for CoOp training over fairface. Created script to launch Clip Zeroshot on fairface Initial draft for Clip evaluation on psychological labels

---
+++ zeroshot_fairface.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# custom config
+DATA=/work/tesi_aonori/CoOp_datasets
+TRAINER=ZeroshotCLIP
+DATASET=fairface
+CFG=$1  # rn50, rn101, vit_b32 or vit_b16
+FAIRFACECLASS=$2  # select label class for FairFace
+
+/homes/aonori/dassl_interpreter.sh /homes/aonori/Tirocinio/CoOp/train.py \
+--root ${DATA} \
+--trainer ${TRAINER} \
+--dataset-config-file configs/datasets/${DATASET}.yaml \
+--config-file configs/trainers/CoOp/${CFG}.yaml \
+--output-dir output/${TRAINER}/${DATASET}/${CFG}/${FAIRFACECLASS}/ \
+--eval-only \
+FAIRFACECLASS ${FAIRFACECLASS}
