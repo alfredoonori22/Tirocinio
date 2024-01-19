7d3758b4e7f5770a9f89b399acc71486c84b2908
alfredoonori22 <alfredoonori45@gmail.com>
2024-01-19 16:07:48 +0100

    Code rearrangment for psichologist test

--- psichologist_handcrafted_prompts.py
+++ psichologist_handcrafted_prompts.py
@@ -1,33 +1,9 @@
 import torch
-import datasets
 import argparse
-import pandas as pd
-from PIL import Image
 from CoOp import clip
 from tqdm import tqdm
 from global_variables import *
-from matplotlib import pyplot as plt
-from psichologist_fairface_prompts import Face, create_Heatmap
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
-
-def classify_zeroshot(faces, prompt_features, class_labels):
-  labels, predictions = [], []
-
-  for face in tqdm(faces):
-    # distribuzione di probabilità che misura la similarità tra le caratteristiche dell'immagine e i prompt di testo
-    similarity = (100.0 * face.image_features @ prompt_features.T).softmax(dim=-1)
-
-    # restituirà il valore massimo (value) e l'indice corrispondente (index)
-    [value], [index] = similarity[0].topk(1)
-
-    #  conterrà l'etichetta di classe prevista per l'immagine in base al confronto con i prompt di testo
-    prediction = class_labels[index]
-
-    labels.append(face.label)
-    predictions.append(prediction)
-
-  return labels, predictions
 
 if __name__ == "__main__":
 
@@ -52,12 +28,12 @@ if __name__ == "__main__":
 
     with torch.no_grad():
         # prende i prompt di testo tokenizzati e li converte in rappresentazioni numeriche (embedding)
-        prompt_features = model.encode_text(tokenized_prompts)
-        prompt_features /= prompt_features.norm(dim=-1, keepdim=True)
+        encoded_prompts = model.encode_text(tokenized_prompts)
+        encoded_prompts /= encoded_prompts.norm(dim=-1, keepdim=True)
 
         faces = [Face(face) for face in tqdm(fairface)]
         # Run clip with the faces and the different prompt (find the nearest one to the proposed image)
-        fairface_labels, predictions = classify_zeroshot(faces, prompt_features, class_labels)
+        fairface_labels, predictions = classify(faces, encoded_prompts, class_labels)
 
     # Create the heatmap to visualize the data
     percentage_matrix = create_Heatmap(fairface_labels, predictions, coop=False)
