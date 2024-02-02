78fd972de786d955a53f1ff0c8c0c220287a7b64
alfredoonori22 <alfredoonori45@gmail.com>
2024-02-02 02:12:31 +0100

    generalized the plot creation, now all the three classes (gender, age, race) produce each different plots

--- psychologist_utils.py
+++ psychologist_utils.py
@@ -9,7 +9,7 @@ from CoOp import clip
 from collections import Counter
 
 class Face:
-    def __init__(self, fairface_face, dataset_dir, device, model, preprocess):
+    def __init__(self, fairface_face, category, dataset_dir, device, model, preprocess):
         """
         Initialize a Face object.
 
@@ -20,9 +20,10 @@ class Face:
             model: The model used for encoding the image features.
             preprocess: The preprocessing function for the image.
         """
+        self.category = fairface_face[f'{category}']
+        self.label = f'{self.category}'
         self.race = fairface_face.get('race')
         self.gender = fairface_face.get('gender')
-        self.label = f'{self.race}_{self.gender}'
         self.dataset_dir = dataset_dir
         self.device = device
         self.model = model
@@ -101,11 +102,11 @@ def calculate_category_percentage(matrix, categories, unique_predictions):
     return matrix[:, category_indices].sum(axis=1)
 
 
-def create_Heatmap(unique_labels, unique_predictions, counts, coop=True):
-    matrix = np.zeros((len(unique_labels), len(unique_predictions)))
+def create_Heatmap(unique_labels, complete_labels, counts, category, coop=True):
+    matrix = np.zeros((len(unique_labels), len(complete_labels)))
 
     for i, label in enumerate(unique_labels):
-        for j, pred in enumerate(unique_predictions):
+        for j, pred in enumerate(complete_labels):
             matrix[i, j] = counts.get((label, pred), 0)
 
     row_sums = matrix.sum(axis=1, keepdims=True)
@@ -114,27 +115,25 @@ def create_Heatmap(unique_labels, unique_predictions, counts, coop=True):
     plt.figure(figsize=(10, 8))
     sns.set(font_scale=0.7)
     ax = sns.heatmap(percentage_matrix, annot=True, fmt='.2f', cmap='Greens',
-                     xticklabels=unique_predictions,
+                     xticklabels=complete_labels,
                      yticklabels=unique_labels,
                      annot_kws={"size": 8})
     plt.xlabel('Predicted')
     plt.ylabel('True')
     plt.title('Prediction Distribution Percentage')
     if coop:
-        print("Saving heatmap in for fairface")
-        # plt.savefig('images/fairface_age/heatmap_fairface.jpg')
+        plt.savefig(f'images/fairface/{category}/heatmap_fairface.jpg')
     else:
-        print("Saving heatmap for handcrafted")
-        # plt.savefig('images/handcrafted/heatmap.jpg')
+        plt.savefig(f'images/handcrafted/{category}/heatmap.jpg')
 
     return percentage_matrix
 
 
-def create_Combined_Matrix(percentage_matrix, unique_labels, unique_predictions, coop=True):
+def create_Combined_Matrix(percentage_matrix, unique_labels, category, coop=True):
     # Compute category percentage
-    competence_percentage = calculate_category_percentage(percentage_matrix, competence_categories, unique_predictions)
-    warmth_percentage = calculate_category_percentage(percentage_matrix, warmth_categories, unique_predictions)
-    morality_percentage = calculate_category_percentage(percentage_matrix, morality_categories, unique_predictions)
+    competence_percentage = calculate_category_percentage(percentage_matrix, competence_categories, complete_labels)
+    warmth_percentage = calculate_category_percentage(percentage_matrix, warmth_categories, complete_labels)
+    morality_percentage = calculate_category_percentage(percentage_matrix, morality_categories, complete_labels)
 
     combined_matrix = np.vstack((competence_percentage, warmth_percentage, morality_percentage)).T
 
@@ -147,84 +146,20 @@ def create_Combined_Matrix(percentage_matrix, unique_labels, unique_predictions,
     plt.tight_layout()
 
     if coop:
-        print("Saving combined matrix for fairface")
-        # plt.savefig('images/fairface_age/combmatr_fairface.jpg')
+        plt.savefig(f'images/fairface/{category}/combmatr_fairface.jpg')
     else:
-        print("Saving combined matrix for handcrafted")
-        # plt.savefig('images/handcrafted/combmatr.jpg')
+        plt.savefig(f'images/handcrafted/{category}/combmatr.jpg')
 
     return combined_matrix
 
 
-def gender_Polarization(percentage_matrix, unique_labels, unique_predictions, coop=True):
-    competence_percentage = calculate_category_percentage(percentage_matrix, competence_categories, unique_predictions)
-    warmth_percentage = calculate_category_percentage(percentage_matrix, warmth_categories, unique_predictions)
-    morality_percentage = calculate_category_percentage(percentage_matrix, morality_categories, unique_predictions)
-
-    # We will first split the unique_labels into male and female and calculate the differences
-    male_indices = [i for i, label in enumerate(unique_labels) if 'Male' in label]
-    female_indices = [i for i, label in enumerate(unique_labels) if 'Female' in label]
-
-    # Calculate differences
-    competence_diff = competence_percentage[male_indices] - competence_percentage[female_indices]
-    warmth_diff = warmth_percentage[male_indices] - warmth_percentage[female_indices]
-    morality_diff = morality_percentage[male_indices] - morality_percentage[female_indices]
-
-    # Create a dataframe
-    diff_df = pd.DataFrame({
-        'Ethnicity': [label.rsplit('_', 1)[0] for label in unique_labels if 'Male' in label],
-        # Assuming male labels are in even indices
-        'Competence': competence_diff,
-        'Warmth': warmth_diff,
-        'Morality': morality_diff
-    })
-
-    # Melt the dataframe for seaborn
-    diff_melted = diff_df.melt(id_vars='Ethnicity', var_name='Category', value_name='Percentage Difference')
-
-    # Plotting
-    plt.figure(figsize=(10, 6))
-    sns.barplot(x='Ethnicity', y='Percentage Difference', hue='Category', data=diff_melted)
-
-    # Customize the axes and background
-    plt.axhline(0, color='gray', linestyle='--')
-    plt.gca().set_ylim([-20, 20])
-
-    center_x_position = len(diff_df) / 2
-    plt.text(center_x_position, plt.gca().get_ylim()[1] * 0.9, 'Male', ha='center', va='center', fontsize=12)
-    plt.text(center_x_position, plt.gca().get_ylim()[0] * 0.9, 'Female', ha='center', va='center', fontsize=12)
-
-    # Enhance legibility
-    plt.xticks(rotation=45)
-    plt.xlabel('Ethnicity')
-    plt.ylabel('Percentage Difference (Male - Female)')
-    plt.title('Gender Polarization by Ethnicity in Competence, Warmth, and Morality')
-
-    # Move the legend to the side
-    plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
-    plt.tight_layout()
-
-    if coop:
-        print("Saving gender polarization for fairface")
-        # plt.savefig('images/fairface_age/genderpol_fairface.jpg')
-    else:
-        print("Saving gender polarization for handcrafted")
-        # plt.savefig('images/handcrafted/genderpol.jpg')
-
-
-def race_Polarization(percentage_matrix, unique_labels, unique_predictions, coop=True):
-    df = pd.DataFrame(percentage_matrix, index=unique_labels, columns=unique_predictions)
+def polarization(percentage_matrix, unique_labels, category, coop=True):
+    df = pd.DataFrame(percentage_matrix, index=unique_labels, columns=complete_labels)
 
     # Create column per categories
     df['Competence'] = df[competence_categories].sum(axis=1)
     df['Warmth'] = df[warmth_categories].sum(axis=1)
     df['Morality'] = df[morality_categories].sum(axis=1)
-
-    # Remove Female/Male
-    df['Race'] = df.index.str.split('_').str[0]
-    df = df.groupby('Race')[['Competence', 'Warmth', 'Morality']].sum()
-    df = df/6
-    #TODO: Meglio dividere per 6 forse, il Competence di Black è semplicemente la somma dei 6 valori (Competent, Intelligent e Skillfull per Black Male e Black Female)
 
     # Compute mean
     category_means = df[['Competence', 'Warmth', 'Morality']].mean()
@@ -235,17 +170,17 @@ def race_Polarization(percentage_matrix, unique_labels, unique_predictions, coop
     # Barplot
     plt.figure(figsize=(12, 8))
     percentage_increase.plot(kind='bar', width=0.8)
-    plt.title('Percentage Difference from Category Mean')
+    plt.title(f'Percentage Difference from Category Mean for {category} class')
     plt.ylabel('Percentage Difference')
-    plt.xlabel('Race')
+    plt.xlabel(f'{category}')
     plt.xticks(rotation=45, ha='right')
     plt.legend(title='Category', bbox_to_anchor=(1.05, 1), loc='upper left')
     plt.tight_layout()
 
     if coop:
-        plt.savefig('images/fairface_race/racepol_fairface_1.jpg')
+        plt.savefig(f'images/fairface/{category}/{category}pol_fairface.jpg')
     else:
-        plt.savefig('images/handcrafted/racepol.jpg')
+        plt.savefig(f'images/handcrafted/{category}/{category}pol.jpg')
 
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -254,6 +189,8 @@ model, preprocess = clip.load(name="ViT-B/32", device=device)
 competence_categories = ['Competent', 'Intelligent', 'Skillful']
 warmth_categories = ['Warm', 'Friendly', 'Likeable']
 morality_categories = ['Honest', 'Sincere', 'Trustworthy']
+
+complete_labels = ['Competent', 'Intelligent', 'Skillful', 'Warm', 'Friendly', 'Likeable', 'Honest', 'Sincere', 'Trustworthy']
 
 fairface = datasets.load_dataset("csv", data_files="/work/tesi_aonori/CoOp_datasets/FairFace/val_labels.csv")['train']
 # fairface = fairface.select(range(1000))
