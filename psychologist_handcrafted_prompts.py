29373d3c005aa3625f982115ae5c77845f0b9c21
alfredoonori22 <alfredoonori45@gmail.com>
2024-02-10 02:37:32 +0100

    Added ensemble of prompts generated by CoOp Code refactoring

--- psychologist_handcrafted_prompts.py
+++ psychologist_handcrafted_prompts.py
@@ -10,43 +10,47 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", type=str, default="ViT-B/32", help="Baseline model for CLIP")
-    parser.add_argument("--category", type=str, default="gender", help="Label category: race/gender/age")
+    parser.add_argument("--category", type=str, default="age", help="Label category: race/gender/age")
     args = parser.parse_args()
 
-    dataset_dir = "/work/tesi_aonori/CoOp_datasets/FairFace/"
     assert os.path.exists(dataset_dir)
 
-    # Label for testing the model
-    labels = {'Competent': 'A photo of a competent person',
-              'Intelligent': 'A photo of a intelligent person',
-              'Skillful': 'A photo of a skillful person',
-              'Warm': 'A photo of a warm person',
-              'Friendly': 'A photo of a friendly person',
-              'Likeable': 'A photo of a likeable person',
-              'Honest': 'A photo of a honest person',
-              'Sincere': 'A photo of a sincere person',
-              'Trustworthy': 'A photo of a trustworthy person' }
+    # Define labels and prompts
+    labels = {
+        'Competent': 'A photo of a competent person',
+        'Intelligent': 'A photo of an intelligent person',
+        'Skillful': 'A photo of a skillful person',
+        'Warm': 'A photo of a warm person',
+        'Friendly': 'A photo of a friendly person',
+        'Likeable': 'A photo of a likeable person',
+        'Honest': 'A photo of a honest person',
+        'Sincere': 'A photo of a sincere person',
+        'Trustworthy': 'A photo of a trustworthy person'
+    }
 
     class_labels = list(labels.keys())
     prompts = list(labels.values())
 
+    # Tokenize prompts
     tokenized_prompts = torch.cat([clip.tokenize(prompt) for prompt in prompts]).to(device)
 
+    # Encode prompt
     with torch.no_grad():
-        # prende i prompt di testo tokenizzati e li converte in rappresentazioni numeriche (embedding)
         encoded_prompts = model.encode_text(tokenized_prompts)
         encoded_prompts /= encoded_prompts.norm(dim=-1, keepdim=True)
 
+        # Classify faces
         faces = [Face(face, args.category, dataset_dir, device, model, preprocess) for face in tqdm(fairface)]
-        # Run clip with the faces and the different prompt (find the nearest one to the proposed image)
         fairface_labels, predictions = classify(faces, encoded_prompts, class_labels)
 
+    # Count and process predictions
     pairs = list(zip(fairface_labels, predictions))
     counts = Counter(pairs)
     unique_labels = sorted(set(fairface_labels))
 
-    # Create the heatmap to visualize the data
-    percentage_matrix = create_Heatmap(unique_labels, complete_labels, counts, args.category, coop=False)
-    combined_matrix = create_Combined_Matrix(percentage_matrix, unique_labels, args.category, coop=False)
+    # Create heatmap and combined matrix
+    percentage_matrix = create_heatmap(unique_labels, labels, counts, args.category)
+    combined_matrix = create_combined_matrix(percentage_matrix, unique_labels, args.category)
 
-    polarization(percentage_matrix, unique_labels, args.category, coop=False)
+    # Calculate polarization
+    polarization(percentage_matrix, unique_labels, args.category)
